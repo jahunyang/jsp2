d9aee495956046b90e864d96a76cb82f26570b20
DJA <DJA@DJA-PC>
2017-08-19 16:11:26 +0900

    sad

---
+++ web_test/src/common/Exam.java
@@ -0,0 +1,12 @@
+package common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		System.out.println(list);
+	}
+}
